26978a19477fc3b86a939b45d9376c565aa59f95
“Bugs5382” <bugs5382@users.noreply.github.com>
2023-12-24 22:56:26 -0500

    feat: updated base files for npm
    * moved gitignore to npm
    * built all tsconfig
    * updated release-drafter.yml
    * updated README.md

---
+++ template/npm/src/api.ts
@@ -0,0 +1,10 @@
+/**
+ * Use to export TypeDoc into Different Namespaces
+ * Example:
+ *
+ * export * as CORE from './index.js' // depending on the package, this might not be needed
+ * export * as OTHER from './other.js' // depending on the package, this might not be needed
+ *
+ * Otherwise, it needs to follow TypeDoc's API generation Examples.
+ *
+ */
