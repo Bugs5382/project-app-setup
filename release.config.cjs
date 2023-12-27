3993e032096568ddc4b846eaff0a958a2f154f65
“Bugs5382” <bugs5382@users.noreply.github.com>
2023-12-27 09:47:44 -0500

    fix: removed release-drafter

--- release.config.cjs
+++ release.config.cjs
@@ -1,5 +1,24 @@
 module.exports = {
-  "extends": "@the-rabbit-hole/semantic-release-config",
+  "verifyConditions": ['@semantic-release/changelog', '@semantic-release/npm', '@semantic-release/git'],
+  "plugins": [
+    '@semantic-release/commit-analyzer',
+    // '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        "changelogFile": "CHANGELOG.md"
+      }
+    ],
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        "assets": ["package.json", "CHANGELOG.md"],
+        "message": "chore(release): ${nextRelease.version} [ci skip]\n\n${nextRelease.notes}"
+      }
+    ],
+    // "@semantic-release/github"
+  ],
   "branches": [
     "main"
   ]
